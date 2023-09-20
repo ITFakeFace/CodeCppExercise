81b448d0bab85c9fd4e2432159673b0da4cdef14
HungDTTS2208004 <117142252+HungDTTS2208004@users.noreply.github.com>
2023-09-20 14:51:06 +0700

    update

--- Ex18/Ex18.cpp
+++ Ex18/Ex18.cpp
@@ -1,20 +1,37 @@
-// Ex18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <iomanip>
+#include <limits>
+#include <random>
+#include <time.h>
+#include <math.h>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string fname;
+    std::cout << "Enter full name: ";
+    std::getline(std::cin,fname);
+    std::vector<std::string> arrSplit;
+
+    while (fname.find(" ") != -1) {
+        arrSplit.push_back(fname.substr(0, fname.find(" ")));
+        fname.erase(0, fname.find(" ") + 1);
+    }
+    for (int i = 0; i < arrSplit.size(); i++) {
+        for (int j = 0; j < arrSplit[i].length(); j++) {
+            arrSplit[i][j] = tolower(arrSplit[i][j]);
+        }
+    }
+    for (int i = 0; i < fname.length(); i++) {
+        fname[i] = tolower(fname[i]);
+    }
+    std::string email;
+    email.append(fname);
+    email.append(".");
+    for (int i = 0; i < arrSplit.size(); i++) {
+        email.append(arrSplit[i]);
+    }
+    email.append("@hoasen.edu.vn");
+    std::cout << "Email: " << email;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
